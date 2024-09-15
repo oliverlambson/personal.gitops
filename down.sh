ec63a5a53908557902f47d2c26cb4aaddfd5a4ee
Oliver Lambson <oliverlambson@gmail.com>
2024-09-15 22:48:19 +0100

    (wip) observability

--- down.sh
+++ down.sh
@@ -2,7 +2,9 @@
 
 docker stack rm registry
 docker stack rm dummy
+docker stack rm observability
 docker stack rm traefik
+docker network rm loki
 docker network rm private
 docker network rm public
 docker swarm leave --force
---
+++ dummy/logmaker.py
@@ -0,0 +1,7 @@
+import time
+import datetime
+
+print('message="logmaker-start"')
+while True:
+    print(f'timestamp="{datetime.datetime.now()}"')
+    time.sleep(1)
