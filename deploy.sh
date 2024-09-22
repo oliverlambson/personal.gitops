b73d719624804b44ab33cab1df44ad24f9e89c63
Oliver Lambson <oliverlambson@gmail.com>
2024-09-23 00:31:18 +0100

    rsync not scp

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,5 @@ if [ $# -ne 1 ]; then
 fi
 service=$1
 
-ssh ollie@oliverlambson.com "rm -rf ~/$service"
-scp -r $service ollie@oliverlambson.com:~/$service
+rsync -av -e ssh --exclude='.env' $service/ ollie@oliverlambson.com:~/$service/
 ssh ollie@oliverlambson.com "bash -c \"./secrets.sh ~/$service/ && ./generate-config.sh -f ~/$service/compose.yaml | docker stack deploy -d -c - $service\""
