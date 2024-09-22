1eccb083c23e4125aeb6e64da2ffeac5ae4366df
Oliver Lambson <oliverlambson@gmail.com>
2024-09-23 00:47:59 +0100

    fix: don't err on 1password inject overwrite

--- secrets.sh
+++ secrets.sh
@@ -4,4 +4,4 @@ set -a
 source $HOME/.env
 set +a
 
-find ${1:-.} -name "*.op" | xargs -n 1 -I FILE sh -c 'echo "Processing $1"; op inject -i "$1" -o "${1%.op}"' _ FILE
+find ${1:-.} -name "*.op" | xargs -n 1 -I FILE sh -c 'echo "Processing $1"; op inject -f -i "$1" -o "${1%.op}"' _ FILE
