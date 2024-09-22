caaf399705d304776650b4a3b414a7b9538bb6be
Oliver Lambson <oliverlambson@gmail.com>
2024-09-23 00:48:20 +0100

    fix: more robust ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,12 @@ if [ $# -ne 1 ]; then
 	echo "Error: No service provided"
 	exit 1
 fi
+
 service=$1
+if [[ "$service" == */ ]]; then
+	echo "Error: Service name should not end with a trailing slash"
+	exit 1
+fi
 
 rsync -av -e ssh --exclude='.env' $service/ ollie@oliverlambson.com:~/$service/
 ssh ollie@oliverlambson.com "bash -c \"./secrets.sh ~/$service/ && ./generate-config.sh -f ~/$service/compose.yaml | docker stack deploy -d -c - $service\""
