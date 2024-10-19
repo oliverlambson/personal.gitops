2a2d2a665679157bc1e38ae76345c4db2a251f84
Oliver Lambson <oliverlambson@gmail.com>
2024-10-19 19:03:17 +0100

    feat: traefik <> cloudflare proxy

--- stacks/traefik/deploy-pre.sh
+++ stacks/traefik/deploy-pre.sh
@@ -9,3 +9,15 @@ if [ "$(stat -c %a acme.json)" != "600" ]; then
 	echo "Setting permissions for acme.json"
 	chmod 600 acme.json
 fi
+
+mkdir -p cloudflare
+
+if [ ! -f cloudflare/cert.pem ]; then
+	echo "Writing origin-certificate to $(pwd)/cloudflare/cert.pem"
+	echo "{{ op://oliverlambson.com/cloudflare-ssl-origin-server/origin-certificate }}" | op inject -f -o cloudflare/cert.pem
+fi
+
+if [ ! -f cloudflare/key.pem ]; then
+	echo "Writing private-key to $(pwd)/cloudflare/key.pem"
+	echo "{{ op://oliverlambson.com/cloudflare-ssl-origin-server/private-key }}" | op inject -f -o cloudflare/cert.pem
+fi
